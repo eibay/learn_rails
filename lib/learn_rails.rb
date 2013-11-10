a1191425f98e4375e9b9e06aa5e7ca2ffe603414
Peter-Jan Celis <pj@celis.org>
2013-11-10 18:20:30 +0700

    simplify codebase by passing the input array to the subclasses

--- lib/learn_rails.rb
+++ lib/learn_rails.rb
@@ -5,16 +5,20 @@ require "active_support/core_ext/string"
 
 module LearnRails
   def self.analyze(*magic)
-    association       = magic.join(' ')
-    association_type  = magic[1]
-    if ['belongs_to', 'has_one', 'has_many'].include? association_type
-      LearnRails::Associations.send(association_type, association)
+    if is_association? magic
+      association_type  = magic[1]
+      LearnRails::Associations.send(association_type, magic)
     else
       error_message
     end
   end
 
   private
+
+  def self.is_association?(magic)
+    potential_association = magic[1]
+    ['belongs_to', 'has_one', 'has_many'].include? potential_association
+  end
 
   def self.error_message
     <<-error.strip_heredoc
--- spec/learn_rails/associations_spec.rb
+++ spec/learn_rails/associations_spec.rb
@@ -3,15 +3,15 @@ require 'learn_rails'
 describe LearnRails::Associations do
   context "should return should return the right commented out ruby code for association:" do
     it "Task belongs_to :user" do
-      LearnRails::Associations.belongs_to("Task belongs_to :user").should eql belongs_to_heredoc
+      LearnRails::Associations.belongs_to(%w(Task belongs_to :user)).should eql belongs_to_heredoc
     end
 
     it "User has_one :task" do
-      LearnRails::Associations.has_one("User has_one :task").should eql has_one_heredoc
+      LearnRails::Associations.has_one(%w(User has_one :task)).should eql has_one_heredoc
     end
 
     it "User has_many :tasks" do
-      LearnRails::Associations.has_many("User has_many :tasks").should eql has_many_heredoc
+      LearnRails::Associations.has_many(%w(User has_many :tasks)).should eql has_many_heredoc
     end
   end
 
