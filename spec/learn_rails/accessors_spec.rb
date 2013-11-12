88bde7ecef7ff4e4cd60166909aa9fe24bf40098
Peter-Jan Celis <pj@celis.org>
2013-11-12 19:14:39 +0700

    Refactor the three accessor methods into a single #code_for method

--- spec/learn_rails/accessors_spec.rb
+++ spec/learn_rails/accessors_spec.rb
@@ -3,27 +3,27 @@ require 'learn_rails'
 describe LearnRails::Accessors do
   context "should return the right commented out ruby code for accessor:" do
     it "attr_reader :name" do
-      LearnRails::Accessors.reader(%w(attr_reader :name)).should eql attr_reader_code
+      LearnRails::Accessors.code_for(%w(attr_reader :name)).should eql attr_reader_code
     end
 
     it "attr_writer :name" do
-      LearnRails::Accessors.writer(%w(attr_writer :name)).should eql attr_writer_code
+      LearnRails::Accessors.code_for(%w(attr_writer :name)).should eql attr_writer_code
     end
 
     it "attr_accessor :name" do
-      LearnRails::Accessors.accessor(%w(attr_accessor :name)).should eql attr_accessor_code
+      LearnRails::Accessors.code_for(%w(attr_accessor :name)).should eql attr_accessor_code
     end
 
     it "attr_reader :name, :another" do
-      LearnRails::Accessors.reader(%w(attr_reader :name, :another)).should eql attr_reader_two_attributes_code
+      LearnRails::Accessors.code_for(%w(attr_reader :name, :another)).should eql attr_reader_two_attributes_code
     end
 
     it "attr_writer :name, :another" do
-      LearnRails::Accessors.writer(%w(attr_writer :name, :another)).should eql attr_writer_two_attributes_code
+      LearnRails::Accessors.code_for(%w(attr_writer :name, :another)).should eql attr_writer_two_attributes_code
     end
 
     it "attr_accessor :name, :another" do
-      LearnRails::Accessors.accessor(%w(attr_accessor :name, :another)).should eql attr_accessor_two_attributes_code
+      LearnRails::Accessors.code_for(%w(attr_accessor :name, :another)).should eql attr_accessor_two_attributes_code
     end
   end
 
