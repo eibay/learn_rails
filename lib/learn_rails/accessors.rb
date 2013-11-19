aa05e934a464dcf9f22977294cc0b7e10f6f4ba0
Peter-Jan Celis <pj@celis.org>
2013-11-20 01:11:27 +0700

    remove initialization method from accessors

--- lib/learn_rails/accessors.rb
+++ lib/learn_rails/accessors.rb
@@ -4,12 +4,11 @@ module LearnRails
       attributes = attributes_from accessor
 
       code = <<-code.gsub(/^\s+/, '')
-        #{initialize_method_for attributes}
       code
 
       attributes.each do |attribute|
-        code << getter_method_for(attribute) if ['reader', 'accessor'].include? accessor_type(accessor)
-        code << setter_method_for(attribute) if ['writer', 'accessor'].include? accessor_type(accessor)
+        code << getter_method_for(attribute, code) if ['reader', 'accessor'].include? accessor_type(accessor)
+        code << setter_method_for(attribute, code) if ['writer', 'accessor'].include? accessor_type(accessor)
       end
 
       code
@@ -25,26 +24,18 @@ module LearnRails
       accessor.drop(1).map { |attr| attr.delete(',').delete(':') }.delete_if(&:empty?)
     end
 
-    def self.initialize_method_for attributes
+    def self.getter_method_for attribute, code
       <<-code.gsub(/^\s+/, '')
-        # def initialize(#{attributes.join ', '})
-        #{attributes.map { |attr| "#  @#{attr} = #{attr}" }.join("\n")}
-        # end
-      code
-    end
-
-    def self.getter_method_for attribute
-      <<-code.gsub(/^\s+/, '')
-        #
+        #{"#\n" unless code.empty?}
         # def #{attribute}
         #  @#{attribute}
         # end
       code
     end
 
-    def self.setter_method_for attribute
+    def self.setter_method_for attribute, code
       <<-code.gsub(/^\s+/, '')
-        #
+        #{"#\n" unless code.empty?}
         # def #{attribute}=(value)
         #  @#{attribute} = value
         # end
