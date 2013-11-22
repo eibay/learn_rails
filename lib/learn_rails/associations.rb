ee68742f2b1ed0411e388d06d3e5ca06fd50ad8f
Peter-Jan Celis <pj@celis.org>
2013-11-23 00:02:34 +0700

    Refactor away nested if statement in Associations#detect_conditions_option

--- lib/learn_rails/associations.rb
+++ lib/learn_rails/associations.rb
@@ -27,32 +27,31 @@ module LearnRails
     end
 
     def self.detect_conditions_option association
-      if association.include? "conditions"
-        index = association.index "conditions"
-        conditions = []
-        start = false
-        association[index..-1].each do |opt|
-          if opt.include?('{')
-            start = true
-            association.delete(opt)
-            conditions << opt.delete('{') if opt.length > 1
-            next
-          end
-
-          if opt.include?('}')
-            start = false
-            association.delete(opt)
-            conditions << opt.delete('}') if opt.length > 1
-            next
-          end
-
-          if start
-            association.delete(opt)
-            conditions << opt
-          end
+      return association unless association.include? "conditions"
+      index = association.index "conditions"
+      conditions = []
+      start = false
+      association[index..-1].each do |opt|
+        if opt.include?('{')
+          start = true
+          association.delete(opt)
+          conditions << opt.delete('{') if opt.length > 1
+          next
         end
-        association.insert(index+1, Hash[*conditions.flatten])
+
+        if opt.include?('}')
+          start = false
+          association.delete(opt)
+          conditions << opt.delete('}') if opt.length > 1
+          next
+        end
+
+        if start
+          association.delete(opt)
+          conditions << opt
+        end
       end
+      association.insert(index+1, Hash[*conditions.flatten])
     end
   end
 end
