a21e8d592cebfca12f5c2c013d911e8d87b610b4
Peter-Jan Celis <pj@celis.org>
2013-11-23 16:27:03 +0700

    Use the right contexts and spec descriptions for rspec tests

--- spec/learn_rails/accessors_spec.rb
+++ spec/learn_rails/accessors_spec.rb
@@ -9,44 +9,60 @@ describe LearnRails::Accessors do
   end
 
   context "attr" do
-    context "readers" do
-      it "attr_reader :name" do
-        LearnRails::Accessors.code_for(%w(attr_reader :name)).should eql attr_reader_code
+    context "reader" do
+      context "with one attribute" do
+        it "should return the correct code" do
+          LearnRails::Accessors.code_for(%w(attr_reader :name)).should eql attr_reader_code
+        end
       end
 
-      it "attr_reader :name, :another" do
-        LearnRails::Accessors.code_for(%w(attr_reader :name, :another)).should eql attr_reader_two_attributes_code
-      end
-    end
-
-    context "writers" do
-      it "attr_writer :name" do
-        LearnRails::Accessors.code_for(%w(attr_writer :name)).should eql attr_writer_code
-      end
-
-      it "attr_writer :name, :another" do
-        LearnRails::Accessors.code_for(%w(attr_writer :name, :another)).should eql attr_writer_two_attributes_code
+      context "with multiple attributes" do
+        it "should return the correct code" do
+          LearnRails::Accessors.code_for(%w(attr_reader :name, :another)).should eql attr_reader_two_attributes_code
+        end
       end
     end
 
-    context "accessors" do
-      it "attr_accessor :name" do
-        LearnRails::Accessors.code_for(%w(attr_accessor :name)).should eql attr_accessor_code
+    context "writer" do
+      context "with one attribute" do
+        it "should return the correct code" do
+          LearnRails::Accessors.code_for(%w(attr_writer :name)).should eql attr_writer_code
+        end
       end
 
-      it "attr_accessor :name, :another" do
-        LearnRails::Accessors.code_for(%w(attr_accessor :name, :another)).should eql attr_accessor_two_attributes_code
+      context "with multiple attributes" do
+        it "should return the correct code" do
+          LearnRails::Accessors.code_for(%w(attr_writer :name, :another)).should eql attr_writer_two_attributes_code
+        end
+      end
+    end
+
+    context "accessor" do
+      context "with one attribute" do
+        it "should return the correct code" do
+          LearnRails::Accessors.code_for(%w(attr_accessor :name)).should eql attr_accessor_code
+        end
+      end
+
+      context "with multiple attributes" do
+        it "should return the correct code" do
+          LearnRails::Accessors.code_for(%w(attr_accessor :name, :another)).should eql attr_accessor_two_attributes_code
+        end
       end
     end
   end
 
   context "with comma" do
-    it "right after the last attribute" do
-      LearnRails::Accessors.code_for(%w(attr_accessor :name,)).should eql attr_accessor_code
+    context "right after the last attribute" do
+      it "should return the correct code" do
+        LearnRails::Accessors.code_for(%w(attr_accessor :name,)).should eql attr_accessor_code
+      end
     end
 
-    it "seperated by a space" do
-      LearnRails::Accessors.code_for(%w(attr_accessor :name ,)).should  eql attr_accessor_code
+    context "seperated by a space" do
+      it "should return the correct code" do
+        LearnRails::Accessors.code_for(%w(attr_accessor :name ,)).should  eql attr_accessor_code
+      end
     end
   end
 
